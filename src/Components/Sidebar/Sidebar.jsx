735402e3bbae30b0f4669a90387b2c0b2a89028e
PriyaPandey22555 <pp1549660@gmail.com>
2025-09-04 00:26:22 +0530

    first commit

---
+++ src/Components/Sidebar/Sidebar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <div>
+      <div>
+        <div>
+          <img className='w.40' src="https://i.imgur.com/zqpwkLQ.png" alt=""  />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar
